9735ea6456bd677932680ed0db5b8e27e591406c
Hunter Wright <hunter.wright@sjsu.edu>
2018-10-18 16:51:59 -0700

    Test

--- CS-151_GroupProject/src/HelloWorld.java
+++ CS-151_GroupProject/src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld
 {
 	   public static void main(String[] args)
 	   {
+		   //Print stuff
 		   System.out.println("Hello World");
 		   System.out.println("Goodbye World");
 	   }
