f6d0bc34b035b7afcdbc618ac44cc652b564594e
Daniel_Fonyo <daniel.fonyo@sjsu.edu>
2018-10-23 11:35:42 -0700

    added bg and started stopwatch

---
+++ CS-151_GroupProject/src/BackgroundPlane.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class BackgroundPlane {
+    public BackgroundPlane(){
+        JFrame frame = new JFrame("Drone Game");
+
+
+        ImageIcon bg = new ImageIcon("assets/background_sky.png");
+        JLabel imgLabel = new JLabel(bg);
+
+        frame.add(imgLabel);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setSize(1000,600);
+
+    }
+}
