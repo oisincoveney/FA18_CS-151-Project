ef6435c8f99aad01610154e471595eab856e582c
Hunter Wright <hunter.wright@sjsu.edu>
2018-10-25 10:54:53 -0700

    pkg main for initial midterm code

--- CS-151_GroupProject/src/main/GameObject.java
+++ CS-151_GroupProject/src/main/GameObject.java
@@ -1,3 +1,5 @@
+package main;
+
 public interface GameObject
 {
 	void draw(java.awt.Graphics g);
--- CS-151_GroupProject/src/main/ObjectManager.java
+++ CS-151_GroupProject/src/main/ObjectManager.java
@@ -1,3 +1,5 @@
+package main;
+
 public interface ObjectManager
 {
 	void paint(java.awt.Graphics g);
